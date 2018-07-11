4ded0fe721f6b059ca65742817043d98516a6840
Voloshyna Jelena <voloshynajelena@gmail.com>
2018-07-11 23:34:42 +0300

    add fix

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .use(express.static(path.join(__dirname, 'dist')))
   .get('/', (req, res) => res.render('dist/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
