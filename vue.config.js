6ab7ec379264f970b22235fa46d26bbfe65d4627
Volochyn Dmytro <woloshindima@MacBook-Air-Volochyn.local>
2018-07-21 22:19:27 +0300

    add new webpack confige

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    configureWebpack: {
+        devServer: {
+
+            compress: true,
+        
+            disableHostCheck: true
+        
+         }     
+    }
+  }
