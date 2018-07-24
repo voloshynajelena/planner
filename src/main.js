dcc6228ec94fa2cc9b39d0f167bb5f735645680a
Voloshyna Jelena <voloshynajelena@gmail.com>
2018-07-24 15:22:31 +0300

    automessage

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
+ 
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
